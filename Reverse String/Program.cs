326636d59c18170155185713091c8a6113612827
SVEN-HSU <sven.hsu@gmail.com>
2016-11-02 00:15:49 +0800

    20161101

---
+++ Reverse String/Program.cs
@@ -0,0 +1,34 @@
+﻿//https://leetcode.com/problems/reverse-string/
+//accepted :)
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(reverseString("12345"));
+            Console.Read();
+        }
+
+        public static string reverseString(string s)
+        {
+            if (s == null)
+            {
+                return null;
+            }
+
+            if (s.Length == 0)
+            {
+                return "";
+            }
+
+            return new string(s.Reverse().ToArray());
+        }
+    }
+}
